ae12385814a0d568404d58fa2ae38ddbfd70cfc6
Nakkcad <115458518+Nakkcad@users.noreply.github.com>
2025-05-18 00:41:56 +0700

    added bookingseat

---
+++ src/model/BookingSeat.java
@@ -0,0 +1,22 @@
+// BookingSeat.java
+package model;
+
+public class BookingSeat {
+    private int bookingId;
+    private int screeningSeatId;
+    public int getBookingId() {
+    return bookingId;
+}
+
+public void setBookingId(int bookingId) {
+    this.bookingId = bookingId;
+}
+
+public int getScreeningSeatId() {
+    return screeningSeatId;
+}
+
+public void setScreeningSeatId(int screeningSeatId) {
+    this.screeningSeatId = screeningSeatId;
+}
+}
