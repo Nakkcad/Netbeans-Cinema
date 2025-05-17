0413f624624588b862615a24eb5963618227cc57
Nakkcad <115458518+Nakkcad@users.noreply.github.com>
2025-05-17 15:59:30 +0700

    added userid

--- src/Utils/UserSession.java
+++ src/Utils/UserSession.java
@@ -1,35 +1,40 @@
 package Utils;
 
 public class UserSession {
-    private static UserSession instance;
+    private static String username;
+    private static String role;
+    private static int userid;
 
-    private String username;
-    private int userId; // <-- Add this line
-
-    private UserSession(String username, int userId) {
-        this.username = username;
-        this.userId = userId; // <-- And this
-    }
-
-    public static void createSession(String username, int userId) {
-        if (instance == null) {
-            instance = new UserSession(username, userId);
-        }
-    }
-
-    public static UserSession getInstance() {
-        return instance;
-    }
-
-    public String getUsername() {
+    public static String getUsername() {
         return username;
     }
 
-    public int getUserId() {
-        return userId; // <-- Add this getter
+    public static void setUsername(String username) {
+        UserSession.username = username;
     }
 
+    public static String getRole() {
+        return role;
+    }
+
+    public static void setRole(String role) {
+        UserSession.role = role;
+    }
+    
+    
+        public static int getUserId() {
+        return userid;
+    }
+
+    public static void setUserId(int userid) {
+        UserSession.userid = userid;
+    }
+    
+    
+
     public static void clearSession() {
-        instance = null;
+        username = null;
+        role = null;
+        userid = -1;
     }
 }
